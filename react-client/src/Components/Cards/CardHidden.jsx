f1149e4db7fe26eb3ee3e2abdebf5074ea5d2da7
icowen <34110641+icowen@users.noreply.github.com>
2018-08-20 16:33:59 -0500

    Updated tests for image

--- react-client/src/Components/Cards/CardHidden.jsx
+++ react-client/src/Components/Cards/CardHidden.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import cardback from './cardback.png';
-export default class Card extends Component {
+
+export default class CardHidden extends Component {
     render() {
         return (
-            <img src={cardback} />
+            <img className={'card-back'} src={cardback} />
         )
     }
 }
--- react-client/src/Components/Cards/CardHidden.test.jsx
+++ react-client/src/Components/Cards/CardHidden.test.jsx
@@ -5,7 +5,7 @@ import CardHidden from './CardHidden';
 const card = shallow(<CardHidden />);
 
 describe('card', function () {
-    it('should say hidden', function () {
-        expect(card.text()).toEqual('Hidden!');
+    it('should say have an image', function () {
+        expect(card.find('.card-back').length).toEqual(1);
     });
 });
