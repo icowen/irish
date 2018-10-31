a80027076062e36f0224cbae6c47c1216cfaf736
icowen <34110641+icowen@users.noreply.github.com>
2018-10-30 22:50:52 -0500

    Getting somewhere with the deck.

--- react-client/src/Components/CardDeck.jsx
+++ react-client/src/Components/CardDeck.jsx
@@ -1,41 +1,50 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import Card from "./Cards/Card";
 
 export default class CardDeck extends Component {
     constructor () {
-        super(props);
+        super();
         this.state = {
-            availableCards: this.initializeDeck,
-            cardCount: 52
+            availableCards: this.initializeDeck()
         };
+        this.getCards = this.getCards.bind(this);
     }
+
     initializeDeck () {
         const suits = ['S', 'C', 'H', 'D'];
         const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
         const cardDeck = [];
-        for (suit in suits) {
-            for (value in values) {
-                cardDeck.push(suit + value);
+        for (let suit in suits) {
+            for (let value in values) {
+                cardDeck.push(<Card key={suit + value} value={Number(value)} suit={suit} />);
             }
         }
+        this.shuffleDeck(cardDeck);
         return cardDeck;
+    }
+
+    shuffleDeck(deck) {
+        for (let i = deck.length - 1; i > 0; i--) {
+            let randomIndex = Math.floor(Math.random() * i);
+
+            let temp = deck[i];
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
     }
 
     getCards (numberOfCards) {
         const index = [];
         const returnCards = [];
         for(let i = 0; i < numberOfCards; i++) {
-            let selected = Math.round(Math.random() * this.state.cardCount);
+            let selected = Math.round(Math.random() * 52);
+            console.log(selected);
             index.push(selected);
-            returnCards.push(this.state.availableCards[card]);
+            returnCards.push(<div>this.state.availableCards[selected]</div>);
             this.setState({availableCards: this.state.availableCards.splice(selected, 1)});
         }
         return returnCards;
     }
 
-    render() { return ( this.getCards(this.props.numberOfCards) ) }
+    render() { return( this.state.availableCards ) }
 }
-
-CardDeck.propTypes = {
-    numberOfCards: PropTypes.number
-};
--- react-client/src/Components/CardDeck.test.jsx
+++ react-client/src/Components/CardDeck.test.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import CardDeck from "./CardDeck";
+import renderer from 'react-test-renderer';
 
 describe('Card deck', function () {
-    const component = <CardDeck />;
+    const component = renderer.create(<CardDeck />);
 
     it('should start out with 52 cards', function () {
-        expect(component.props().length).toEqual(52);
+        expect(component.getInstance().state.availableCards.length).toEqual(52);
     });
 });
